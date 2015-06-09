bbbf74f58358958564b19b312fc5df08ecf3a064
shpark727 <shpark727@gmail.com>
2015-06-09 09:42:29 +0000

    carrierwave gem사용 준비하기

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	validates :category, :inclusion=>{:in => ["한식","일식","중식","양식","디저트"], :message =>"한식일식중식양식디저트 중 하나를 선택하셔야합니다."}
 	validates :title, :presence =>{ :message =>"제목을 반드시 입력하세요."}
+	mount_uploader :image, ImageUploader
 	
 end
